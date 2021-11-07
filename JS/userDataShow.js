e55abdfba58bb8e29a1a859cdaba6c745d8a7a6a
Rollfreeze <Goronos2000@gmail.com>
2021-11-07 17:43:55 +0300

    fix

--- JS/userDataShow.js
+++ JS/userDataShow.js
@@ -29,15 +29,18 @@ $.getJSON('https://json.geoiplookup.io/?callback=?', function (data) {
     element.innerHTML = country;
 });
 
-$.getJSON('http://www.geoplugin.net/json.gp', function (data) {
-    // console.log(JSON.stringify(data, null, 2));
-    arrDataElements2 = JSON.parse(JSON.stringify(data));
+let apiKey = 'd42dc0e5b04445acb757dc4de23386d5';
+$.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+  console.log(JSON.stringify(data, null, 2));
 
-    const ipV4 = arrDataElements2.geoplugin_request;
-    const region = arrDataElements2.geoplugin_region;
-    const city = arrDataElements2.geoplugin_city;
-    const timeZone = arrDataElements2.geoplugin_timezone;
-    const coordinates = arrDataElements2.geoplugin_latitude + " " + arrDataElements2.geoplugin_longitude;
+  arrDataElements2 = JSON.parse(JSON.stringify(data));
+
+    const ipV4 = arrDataElements2.ip_address;
+    const region = arrDataElements2.region;
+    const city = arrDataElements2.city;
+    const timeZone = arrDataElements2.timezone.abbreviation;
+    const coordinates = arrDataElements2.latitude + " " + arrDataElements2.longitude;
+    const vpn = arrDataElements2.security.is_vpn;
 
     element = document.querySelector('#ipV4');
     element.innerHTML = ipV4;
@@ -53,7 +56,39 @@ $.getJSON('http://www.geoplugin.net/json.gp', function (data) {
 
     element = document.querySelector('#coordinates');
     element.innerHTML = coordinates;
+
+    element = document.querySelector('#VPN');
+    element.innerHTML = vpn;
 });
+
+// $.getJSON('http://www.geoplugin.net/json.gp', function (data) {
+//     // console.log(JSON.stringify(data, null, 2));
+//     arrDataElements2 = JSON.parse(JSON.stringify(data));
+
+//     const ipV4 = arrDataElements2.geoplugin_request;
+//     const region = arrDataElements2.geoplugin_region;
+//     const city = arrDataElements2.geoplugin_city;
+//     const timeZone = arrDataElements2.geoplugin_timezone;
+//     const coordinates = arrDataElements2.geoplugin_latitude + " " + arrDataElements2.geoplugin_longitude;
+
+//     element = document.querySelector('#ipV4');
+//     element.innerHTML = ipV4;
+
+//     element = document.querySelector('#region');
+//     element.innerHTML = region;
+
+//     element = document.querySelector('#city');
+//     element.innerHTML = city;
+
+//     element = document.querySelector('#timeZone');
+//     element.innerHTML = timeZone;
+
+//     element = document.querySelector('#coordinates');
+//     element.innerHTML = coordinates;
+
+//     element = document.querySelector('#VPN');
+//     element.innerHTML = coordinates;
+// });
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.ipdata.co/?api-key=9ab994ce35f473124877ffa978a6dc2c485a4a3ca94a0d95f954e93c');
@@ -63,7 +98,7 @@ request.onreadystatechange = function () {
         // console.log(this.responseText);
 
         let threatData = JSON.parse(this.responseText);
-        console.log(threatData.threat);
+        // console.log(threatData.threat);
 
         element = document.querySelector('#Proxy');
         element.innerHTML = threatData.threat.is_proxy;
